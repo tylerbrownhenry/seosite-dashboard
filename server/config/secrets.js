6e3f6b3cd1bc8fc7d11f8d045f74e528b712f047
Tyler Henry <tyler@milmerry.com>
2017-01-29 17:53:44 -0500

    ‘test’

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,17 +1,14 @@
 module.exports = {
+    var mongoURL = null;
+    if(process.env.MONGO_URL && process.env.MONGO_DB){
+        mongoURL = 'mongodb://' + process.env.MONGO_URL + '/' + process.env.MONGO_DB
+    } else {
+        console.log('Either set MONGO_URL AND MONGO_DB in your .env file or set those config vars in heroku dashboard');
+    }
 
-  db: process.env.MONGODB || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://seodoctor:ep1uTSDD81FMOLCd0zbE@ds159208.mlab.com:59208/stripe-users',
-
-    // db: {
-    //   "service": "mongodb",
-    //   "host": "seodoctor:ep1uTSDD81FMOLCd0zbE@ds159208.mlab.com:59208",
-    //   "database": "stripe-users"
-    // },
-
+  db: process.env.MONGODB || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || mongoURL,
     apiToken: '530d0C$3cr3T',
-
     sessionSecret: process.env.SESSION_SECRET || '530d0C$3cr3T',
-
   mailgun: {
     apiKey: process.env.MAILGUN_API_KEY || '',
     domain: process.env.MAILGUN_DOMAIN || ''
@@ -41,6 +38,5 @@ module.exports = {
       }
     }
   },
-
   googleAnalytics: process.env.GOOGLE_ANALYTICS || ''
 };
