d1966c338ed315f17c824a2565dfba7762bdbe2a
Tyler Henry <tyler@milmerry.com>
2017-01-29 14:28:45 -0500

    ‘test’

--- api-requests/requests.js
+++ api-requests/requests.js
@@ -43,10 +43,10 @@ function requests(socket){
 
         var options = {
           hostname: process.env.API_HOST || 'localhost',
-          port: 21545,
+          // port: 21545,
           // path: '/api/v1/queue',
           path: '/api/',
-          method: 'POST',
+          method: 'GET',
           headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Content-Length': Buffer.byteLength(postData)
