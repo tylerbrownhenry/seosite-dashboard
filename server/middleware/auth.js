bc4589c93eb0f0fda6553403e91e2204065e6db9
Tyler Henry <tyler@milmerry.com>
2017-01-29 14:51:30 -0500

    ‘test’

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -17,6 +17,10 @@ exports.isUnauthenticated = function(req, res, next) {
 };
 
 exports.apiIsUnauthenticated = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+        // res.header('Access-Control-Allow-Headers', 'accept, content-type, application/x-www-form-urlencoded, x-www-form-urlencoded, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+    res.header('Access-Control-Allow-Headers', '*');
   // if (!req.isAuthenticated()){
     return next();
   // }
